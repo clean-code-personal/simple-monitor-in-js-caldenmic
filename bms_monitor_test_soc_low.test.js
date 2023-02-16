16aebc21f39b5dec2a10e8c64745226dd806b020
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-16 15:34:39 +0530

    Update bms_monitor_test_soc_low.test.js

--- bms_monitor_test_soc_low.test.js
+++ bms_monitor_test_soc_low.test.js
@@ -1,4 +1,4 @@
-import { batteryIsOk } from "./bms_monitor_production";
+import { batteryIsOk } from "./bms-monitor";
 import { batteryReporter } from "./bms_monitor_battery_reporter";
 
 test("Ckecking if all the State of charge status is low", () => {
