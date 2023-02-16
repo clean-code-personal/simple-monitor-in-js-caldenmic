ffe3f271a7ce96fd5d7da01c32c5a6b000cfb4e4
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-16 15:26:46 +0530

    Create bms_monitor_test_soc_high.test.js

---
+++ bms_monitor_test_soc_high.test.js
@@ -0,0 +1,7 @@
+import { batteryIsOk } from "./bms_monitor_production";
+import { batteryReporter } from "./bms_monitor_battery_reporter";
+
+test("Ckecking if all the State of Charge status is high", () => {
+    let attribute_status = batteryIsOk(40, 90, 0.7, batteryReporter);
+    expect(attribute_status['soc']).toBe('high');
+});
